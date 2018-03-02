2aa8b7c517c727bb3d274a85a9d0d78d158ba40f
mark-oblack <moblack42@gmail.com>
2018-03-02 13:46:29 -0500

    HTML/CSS changes
    Corrected HTML; began adjusting CSS

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ function cylinderVol(){
 	height=$("#txtHeight").val();
 	if (radius<0 || isNaN(radius) || radius==="" || height<0 || isNaN(height) || height===""){
 		$("#output").removeClass("highlightedGood");
-		$("#output").html("One or more of the values you submitted is invalid. Please try again").addClass("highlightedBad");
+		$("#output").html("One or more of the values you submitted is invalid. Please try again.").addClass("highlightedBad");
 	}
 
 	else{
@@ -21,15 +21,15 @@ $(document).ready(function(e) {
 		cylinderVol();
 	});
 
-	$("#txtRadius,#txtHeight").focus(function(){
-		$(this).addClass("focus");
+	// $("#txtRadius,#txtHeight").focus(function(){
+	// 	$(this).addClass("focus");
 
-	});
+	// });
 
-	$("#txtRadius,#txtHeight").blur(function(){
-		$(this).removeClass("focus");
+	// $("#txtRadius,#txtHeight").blur(function(){
+	// 	$(this).removeClass("focus");
 
-	});
+	// });
 
 	$("#btnReset").click(function(){
 		$("#output").empty();
